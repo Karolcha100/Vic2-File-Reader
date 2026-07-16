1aafe7eb7f15e5be870b35fd5f3f5ce5f4b0794d
Karol Chądzyński <karolcha100@gmail.com>
2026-07-16 19:02:51 +0200

    effect done

--- vic2_script_manegement/statements_effect.py
+++ vic2_script_manegement/statements_effect.py
@@ -1,51 +1,91 @@
-from statements_name import Equation, ListOfEquations
-from typing.types_basic import BasicType
+from abc import ABC, abstractmethod
+
+from statements_name import NameStatement
+from typing.types_basic import BasicType, NumericType, StringType
+from script_node import ScriptNode
 
 
 
 
 
 
-class EffectEquation(Equation):
+class Effect[T: list[Effect[BasicType]] | BasicType](NameStatement, ScriptNode):
     """
-    Effect in form of Equation.
-
-    In Short: 
-    ```
-    name = value
-    ```
-
-    :param name: Name of Effect
-    :type name: str
-
-    :param value: Value assigned to Effect
-    :type value: BasicType
+    Abstract Condition Statement.
     """
+
+    def __init__(self, name: str, value: T) -> None:
+        super().__init__(name)
+        self._value: T = value
+
+    def set_inside(self, value: T) -> None:
+        """
+        
+        """
+        self._value = value
+
+    # @abstractmethod
+    # def execute(self, checked: T) -> bool:
+    #     """
+        
+    #     """
+    #     ...
+
+    def get_inside(self) -> T:
+        """
+        
+        """
+        return self._value
+    
+    @abstractmethod
+    def to_script(self) -> str:
+        ...
+    
+
+
+
+class EffectWithArguments(Effect[list[Effect[BasicType]]]):
+    """
+    
+    """
+
+    def __init__(self, name: str, value: list[Effect[BasicType]]) -> None:
+        super().__init__(name, value)
+
+    # def execute(self, checked: list[Effect[BasicType]]) -> bool:
+    #     return sum(
+    #         [
+    #             cond_value.execute(cond_incoming.get_inside())
+    #             for cond_value, cond_incoming in zip(self._value, checked)
+    #         ]
+    #     ) == len(checked)
+    
+    def to_script(self) -> str:
+        return_str = self._name + " = {\n"
+
+        for cond in self._value:
+            return_str += "\t" + cond.to_script() + "\n"
+
+        return return_str + "}"
+
+
+class EffectEquation(Effect[BasicType]):
+    """
+    
+    """
+
     def __init__(self, name: str, value: BasicType) -> None:
         super().__init__(name, value)
 
-    
+    # def execute(self, checked: BasicType) -> bool:
 
-class EffectWithArguments(ListOfEquations):
-    """
-    Effect in form of List of Equations.
-
-    In Short:
-    ```
-    name = {
-        Equation
-        Equation
-        ...
-    }
-    ```
-    
-    :param name: Name of Effect
-    :type name: str
-
-    :param arguments: Equations assigned to Effect
-    :type arguments: list[Equation]
-
-    :raises Not Enough Equations: When number of provided Arguments is below 1
-    """
-    def __init__(self, name: str, arguments: list[Equation]) -> None:
-        super().__init__(name, arguments)
+    #     if isinstance(checked, NumericType):
+    #         return self._value.get_parsed_value() <= checked.get_parsed_value()
+        
+    #     if isinstance(checked, StringType):
+    #         return self._value.get_parsed_value() == checked.get_parsed_value()
+        
+    #     raise ValueError(f"unrecognized type")
+            
+    def to_script(self) -> str:
+        return f"{self._name} = {self._value.get_raw_value()}"
